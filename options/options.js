6da05f66e4111d27abfb77c06a1faa312642621b
Crowdbotics <team@crowdbotics.com>
2023-07-08 22:21:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cool-thunder-42008",
+  url: "https://cool-thunder-42008.botics.co",
+  api: "https://cool-thunder-42008.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
